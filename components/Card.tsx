09ed837a0302599f2293572e4ba6dfc9dfdbfcf7
doniyor17 <doniyor17ibragimov@gmail.com>
2024-09-25 17:01:56 +0300

    Added animation to play button and updated some styles

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,16 @@
+import { useRef } from "react";
+import {
+  View,
+  Text,
+  Pressable,
+  ImageBackground,
+  Animated,
+  StyleSheet
+} from "react-native";
+
 import AppGradient from "@/components/AppGradient";
-import { View, Text, Pressable, ImageBackground } from "react-native";
 
 import AntDesign from "@expo/vector-icons/AntDesign";
-import IMAGES from "@/constants/BgImages";
 import Colors from "@/constants/Colors";
 
 interface CardProps {
@@ -17,32 +25,60 @@ export default function Card({
   onPressCard,
   onPressPlay
 }: CardProps) {
+  const scaleValue = useRef(new Animated.Value(1)).current;
+  const onPressStart = () => {
+    Animated.spring(scaleValue, {
+      toValue: 0.75, // Scale down slightly
+      useNativeDriver: true
+    }).start();
+  };
+
+  const onPressEnd = () => {
+    Animated.spring(scaleValue, {
+      toValue: 1, // Scale down slightly
+      useNativeDriver: true
+    }).start();
+  };
+
   return (
-    <View className="w-full shadow-md items-center mb-3">
-      <Pressable
-        onPress={onPressCard}
-        className="rounded-3xl"
-      >
+    <View
+      className="w-full items-center mb-4"
+      style={{
+        shadowColor: Colors.dark,
+        shadowOpacity: 30,
+        shadowRadius: 10
+      }}
+    >
+      <Pressable onPress={onPressCard}>
         <ImageBackground
           source={{ uri: imageSrc }}
           resizeMode="cover"
           className="!rounded-lg"
         >
           <View className="w-[300px] h-[300px] rounded-lg relative">
-            <AppGradient colors={["rgba(0, 0, 0, 0.8)", "rgba(0, 0, 0, 0.3)"]}>
+            <AppGradient colors={["rgba(0, 0, 0, 0.5)", "rgba(0, 0, 0, 0.7)"]}>
               <Text className="text-center font-bold text-white mt-3 text-4xl mb-10">
                 {title}
               </Text>
               <Pressable
                 className="z-10 w-24 h-24 rounded-full border-[6px] border-green-700 justify-center items-center absolute bottom-4 right-4"
+                onPressIn={onPressStart}
                 onPress={onPressPlay}
+                onPressOut={onPressEnd}
               >
                 <View className="">
-                  <AntDesign
-                    name="caretright"
-                    size={70}
-                    color={Colors.whitish}
-                  />
+                  <Animated.View
+                    style={[
+                      styles.button,
+                      { transform: [{ scale: scaleValue }] }
+                    ]}
+                  >
+                    <AntDesign
+                      name="caretright"
+                      size={70}
+                      color={Colors.whitish}
+                    />
+                  </Animated.View>
                 </View>
               </Pressable>
             </AppGradient>
@@ -52,3 +88,12 @@ export default function Card({
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "transparent",
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
